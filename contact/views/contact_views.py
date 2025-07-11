ab5e79563ddba6e51c1b62651615e44adeb96474
Alex-Junior-Dev <alexjunior.dev@gmail.com>
2025-07-11 10:44:47 -0300

    Created partials to clean some pages.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -12,6 +12,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
 
@@ -28,10 +29,12 @@ def contact(request, contact_id):
         pk=contact_id,
         show=True,
         )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
 
     context = {
         'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(
