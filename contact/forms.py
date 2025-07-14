c6cf4d3a7243cb774d077fa154e4170be46eeee1
Alex-Junior-Dev <alexjunior.dev@gmail.com>
2025-07-14 11:15:56 -0300

    Added the picture field and the logic to send and show the picture file

--- contact/forms.py
+++ contact/forms.py
@@ -6,52 +6,21 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
-    # first_name = forms.CharField(
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'class': 'classe-a classe-b', 
-    #             'placeholder': 'Aqui veio do init',
-    #         }
-    #     ),
-    #     label='Primeiro Nome',
-    #     help_text='Texto de ajuda para seu usuário',
-    # )
-
-    # Qualquer = forms.CharField(
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'class': 'classe-a classe-b', 
-    #             'placeholder': 'Aqui veio do init',
-    #         }
-    #     ),
-    #     label='Qualquer criado pelo forms',
-    #     help_text='Texto de ajuda para seu usuário',
-    # )
-
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # self.fields['first_name'].widget.attrs.update({
-        #     'class': 'classe-a classe-b',
-        #     'placeholder': 'Aqui veio do init',
-        # })
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*'
+            }
+        )
+    )
 
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone', 'email', 'description',
-            'category',
+            'category', 'picture',
         )
 
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'class': 'classe-a classe-b',
-        #             'placeholder': 'Escreva aqui',
-        #         }
-        #     )
-        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
