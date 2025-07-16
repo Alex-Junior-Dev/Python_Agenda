7d620a5a9a7f0c82dcc5cb7a30719a7f203b22e6
Alex-Junior-Dev <alexjunior.dev@gmail.com>
2025-07-16 08:55:13 -0300

    Finished the Schedule App.

--- contact/forms.py
+++ contact/forms.py
@@ -42,7 +42,8 @@ class ContactForm(forms.ModelForm):
             attrs={
                 'accept': 'image/*'
             }
-        )
+        ),
+        required=False,
     )
 
     class Meta:
